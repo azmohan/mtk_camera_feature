53a99524445a5e135b39950d2f1e1aa3cb2274c3
azmohan <zoumohan@droi.com>
2016-08-01 15:45:37 +0800

    add try camera release runtimeException

--- src/com/freeme/camera/mtkfeature/MtkCamera.java
+++ src/com/freeme/camera/mtkfeature/MtkCamera.java
@@ -20,135 +20,143 @@ import android.hardware.Camera.GestureCallback;
 import android.hardware.Camera.SmileCallback;
 
 public class MtkCamera {
-	private static final String TAG = "AndroidCamera";
+    private static final String TAG = "AndroidCamera";
 
-	protected Camera mCamera = null;
+    protected Camera mCamera = null;
 
-	private class GestureCallbackImpl implements GestureCallback {
-		GestureListener mListener;
+    private class GestureCallbackImpl implements GestureCallback {
+        GestureListener mListener;
 
-		public GestureCallbackImpl(GestureListener listener) {
-			mListener = listener;
-		}
-
-		@Override
-		public void onGesture() {
-			// Because stopGS will set lister is null,
-			// at this time the onGesture is on the road ,so will JE
-			if (mListener != null) {
-				mListener.onGesture();
-			}
-		}
-	}
-
-	private class SmileCallbackImpl implements SmileCallback {
-		SmileListener mListener;
-
-		public SmileCallbackImpl(SmileListener listener) {
-			mListener = listener;
-		}
-
-		@Override
-		public void onSmile() {
-			// Because stopGS will set lister is null,
-			// at this time the onGesture is on the road ,so will JE
-			if (mListener != null) {
-				mListener.onSmile();
-			}
-		}
-	}
-
-	public interface GestureListener {
-		void onGesture();
-	}
-
-	public interface SmileListener {
-		void onSmile();
-	}
-
-	public static void assertError(boolean cond) {
-		if (!cond) {
-			throw new AssertionError();
-		}
-	}
-
-	public MtkCamera(Camera camera) {
-		assertError(null != camera);
-		mCamera = camera;
-	}
-
-	public Camera getInstance() {
-		return mCamera;
-	}
-
-	public void stopSmileDetection() {
-		Log.i(TAG, "[stopSmileDetection]...");
-		mCamera.stopSmileDetection();
-	}
-
-	public void stopGestureDetection() {
-		Log.i(TAG, "[stopGestureDetection]...");
-		mCamera.stopGestureDetection();
-	}
-
-	public void lock() {
-		Log.i(TAG, "[lock]...");
-		mCamera.lock();
-	}
-
-	public Parameters getParameters() {
-		Log.i(TAG, "[getParameters]...");
-		return mCamera.getParameters();
-	}
-
-	public void release() {
-		Log.i(TAG, "[release]...");
-		mCamera.release();
-	}
-
-	public void reconnect() throws IOException {
-		Log.i(TAG, "[reconnect]...");
-		mCamera.reconnect();
-	}
-
-	public void setContext(Context context) {
-		Log.i(TAG, "[setContext]do nothing...");
-	}
-
-	public void setParameters(Parameters params) {
-		Log.i(TAG, "[setParameters]...");
-		mCamera.setParameters(params);
-	}
-
-	public void setSmileCallback(SmileListener listener) {
-		Log.i(TAG, "[setSmileCallback]...");
-		mCamera.setSmileCallback(listener != null ? new SmileCallbackImpl(
-				listener) : null);
-
-	}
-
-	public void setGestureCallback(GestureListener listener) {
-		Log.i(TAG, "[setGestureCallback]...");
-		mCamera.setGestureCallback(listener != null ? new GestureCallbackImpl(
-				listener) : null);
-	}
-
-	public void startSmileDetection() {
-		Log.i(TAG, "[startSmileDetection]...");
-		mCamera.startSmileDetection();
-	}
-
-        public void startGestureDetection() {
-            Log.i(TAG, "[startGestureDetection]...");
-            try {
-                mCamera.startGestureDetection();
-            } catch(Exception e) {
-                Log.i(TAG,"Exception :"+e.toString());
-            }
+        public GestureCallbackImpl(GestureListener listener) {
+            mListener = listener;
         }
 
-	public void unlock() {
-		Log.i(TAG, "[unlock]...");
-		mCamera.unlock();
-	}
+        @Override
+            public void onGesture() {
+                // Because stopGS will set lister is null,
+                // at this time the onGesture is on the road ,so will JE
+                if (mListener != null) {
+                    mListener.onGesture();
+                }
+            }
+    }
+
+    private class SmileCallbackImpl implements SmileCallback {
+        SmileListener mListener;
+
+        public SmileCallbackImpl(SmileListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+            public void onSmile() {
+                // Because stopGS will set lister is null,
+                // at this time the onGesture is on the road ,so will JE
+                if (mListener != null) {
+                    mListener.onSmile();
+                }
+            }
+    }
+
+    public interface GestureListener {
+        void onGesture();
+    }
+
+    public interface SmileListener {
+        void onSmile();
+    }
+
+    public static void assertError(boolean cond) {
+        if (!cond) {
+            throw new AssertionError();
+        }
+    }
+
+    public MtkCamera(Camera camera) {
+        assertError(null != camera);
+        mCamera = camera;
+    }
+
+    public Camera getInstance() {
+        return mCamera;
+    }
+
+    public void stopSmileDetection() {
+        Log.i(TAG, "[stopSmileDetection]...");
+        mCamera.stopSmileDetection();
+    }
+
+    public void stopGestureDetection() {
+        Log.i(TAG, "[stopGestureDetection]...");
+        try {
+            mCamera.stopGestureDetection();
+        } catch(Exception e) {
+            Log.i(TAG,"Exception :"+e.toString());
+        }
+    }
+
+    public void lock() {
+        Log.i(TAG, "[lock]...");
+        mCamera.lock();
+    }
+
+    public Parameters getParameters() {
+        Log.i(TAG, "[getParameters]...");
+        return mCamera.getParameters();
+    }
+
+    public void release() {
+        Log.i(TAG, "[release]...");
+        mCamera.release();
+    }
+
+    public void reconnect() throws IOException {
+        Log.i(TAG, "[reconnect]...");
+        mCamera.reconnect();
+    }
+
+    public void setContext(Context context) {
+        Log.i(TAG, "[setContext]do nothing...");
+    }
+
+    public void setParameters(Parameters params) {
+        Log.i(TAG, "[setParameters]...");
+        mCamera.setParameters(params);
+    }
+
+    public void setSmileCallback(SmileListener listener) {
+        Log.i(TAG, "[setSmileCallback]...");
+        mCamera.setSmileCallback(listener != null ? new SmileCallbackImpl(
+                    listener) : null);
+
+    }
+
+    public void setGestureCallback(GestureListener listener) {
+        Log.i(TAG, "[setGestureCallback]...");
+        try {
+            mCamera.setGestureCallback(listener != null ? new GestureCallbackImpl(
+                        listener) : null);
+        } catch(Exception e) {
+            Log.i(TAG,"Exception :"+e.toString());
+        }
+    }
+
+    public void startSmileDetection() {
+        Log.i(TAG, "[startSmileDetection]...");
+        mCamera.startSmileDetection();
+    }
+
+    public void startGestureDetection() {
+        Log.i(TAG, "[startGestureDetection]...");
+        try {
+            mCamera.startGestureDetection();
+        } catch(Exception e) {
+            Log.i(TAG,"Exception :"+e.toString());
+        }
+    }
+
+    public void unlock() {
+        Log.i(TAG, "[unlock]...");
+        mCamera.unlock();
+    }
 }
